c6cc27d3273c137326e6b2356cc6d3984900d27e
DmitriyAl <dvalbot@mail.ru>
2020-04-16 15:08:09 +0300

    lesson20 listeners were uncommented

--- lesson20/src/main/java/otus/spring/albot/lesson20/listener/AuthorRepoListener.java
+++ lesson20/src/main/java/otus/spring/albot/lesson20/listener/AuthorRepoListener.java
@@ -1,29 +1,31 @@
-//package otus.spring.albot.lesson20.listener;
-//
-//import lombok.AllArgsConstructor;
-//import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
-//import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
-//import org.springframework.stereotype.Component;
-//import otus.spring.albot.lesson20.dao.AuthorRepo;
-//import otus.spring.albot.lesson20.dao.BookRepo;
-//import otus.spring.albot.lesson20.entity.Author;
-//
-//@Component
-//@AllArgsConstructor
-//public class AuthorRepoListener extends AbstractMongoEventListener<Author> {
-//    private AuthorRepo authorRepo;
-//    private BookRepo bookRepo;
-//
-//    @Override
-//    public void onBeforeDelete(BeforeDeleteEvent<Author> event) {
-//        super.onBeforeDelete(event);
-//        String id = event.getSource().get("_id").toString();
-//        authorRepo.findById(id).ifPresent(this::removeAuthorBooks);
-//    }
-//
-//    private void removeAuthorBooks(Author author) {
-//        if (author.getBooks() != null) {
-//            author.getBooks().forEach(b -> bookRepo.deleteById(b.getId()));
-//        }
-//    }
-//}
+package otus.spring.albot.lesson20.listener;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
+import org.springframework.stereotype.Component;
+import otus.spring.albot.lesson20.dao.AuthorRepo;
+import otus.spring.albot.lesson20.dao.BookRepo;
+import otus.spring.albot.lesson20.entity.Author;
+import reactor.core.publisher.Mono;
+
+@Component
+@AllArgsConstructor
+public class AuthorRepoListener extends AbstractMongoEventListener<Author> {
+    private AuthorRepo authorRepo;
+    private BookRepo bookRepo;
+
+    @Override
+    public void onBeforeDelete(BeforeDeleteEvent<Author> event) {
+        super.onBeforeDelete(event);
+        String id = event.getSource().get("_id").toString();
+        authorRepo.findById(id).flatMap(this::removeAuthorBooks).subscribe();
+    }
+
+    private Mono<Void> removeAuthorBooks(Author author) {
+        if (author.getBooks() != null) {
+            return bookRepo.deleteAll(author.getBooks());
+        }
+        return Mono.empty();
+    }
+}
